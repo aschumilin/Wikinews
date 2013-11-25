671701ab3887b4584f786cb33a8782db7cd43652
uxcax <uxcax@rz-y-305.stud.uni-karlsruhe.de>
2013-11-25 15:11:48 +0100

    tes.xml removed

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-wikinewsDump = ET.parse("wikinews source xml")
+wikinewsDump = ET.parse("/media/MYLINUXLIVE/AIFB/datasets/en/enwikinews-20131030-pages-articles.xml")
 for child in wikinewsDump:
 	print child.tag, child.attrib
 # http://docs.python.org/2/library/xml.etree.elementtree.html
