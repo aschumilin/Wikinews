70ef46cb2942f8819f045baed2bc55221372fec4
comp <pilatus@comp.(none)>
2013-11-25 05:45:28 +0100

    xml tutorial reference added

--- parser/__init__.py
+++ parser/__init__.py
@@ -3,4 +3,4 @@ import xml.etree.ElementTree as ET
 wikinewsDump = ET.parse("wikinews source xml")
 for child in wikinewsDump:
 	print child.tag, child.attrib
-
+# http://docs.python.org/2/library/xml.etree.elementtree.html
