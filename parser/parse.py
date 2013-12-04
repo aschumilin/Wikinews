d10626ac4cc91e289eb867a384405b8492437661
pilatus <aschumilin@hotmail.com>
2013-12-04 22:15:30 +0100

    parser fertig

--- parser/parse.py
+++ parser/parse.py
@@ -1,6 +1,16 @@
 import xml.etree.ElementTree as ET
-import time as time 
-from string import join
+from Timer import Timer
+import logging
+
+handlerDebug = logging.FileHandler('./log/debug.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handlerDebug.setFormatter(formatter)
+L = logging.getLogger("wikinews")
+L.setLevel(logging.DDEBUG)
+L.addHandler(handlerDebug)
+
+T = Timer()
+
 
 #def descend(element):
 #	if list(element).__len__()>0:
@@ -11,29 +21,42 @@ from string import join
 
 #dump = ET.parse("/media/MYLINUXLIVE/AIFB/datasets/en/enwikinews-20131030-pages-articles.xml")
 #dump = ET.parse('/media/MYLINUXLIVE/AIFB/datasets/test_xml.xml')
-start1 = time.time()
-dump = ET.parse('../../../en_wikinews_2013-10-30/enwikinews-20131030-pages-articles.xml')
-doc = dump.getroot()
-end1 = time.time()
+#dump = ET.parse('../../../en_wikinews_2013-10-30/enwikinews-20131030-pages-articles.xml')
 
 
+# 1 
+# parse dump and get the root of xml tree
+L.debug("parsing started")
+T.click()
+dump = ET.parse("/media/MYLINUXLIVE/AIFB/datasets/en/enwikinews-20131030-pages-articles.xml")
+wikinews = dump.getroot()
+T.click()
+parsingTime = T.show()
+L.debug("done parsing in: " + parsingTime)
 
 
+# 2 
+# extract the namespace string from the root element tag name
+namespace = wikinews.tag.split("}")[0].strip() + "}"
 
 
-start2 = time.time()
-liste = doc.findall(".//*")
-end2 = time.time()
+# 3
+#
+L.debug("xpathing all page elements")
+T.click()
+liste = wikinews.findall(".//*")
+T.click()
+traversingTime = T.show()
+L.debug("done xpathing pagen in: " + traversingTime)
 
-outputFile = open('../../../en_wikinews_2013-10-30/wikinews-tags.txt', 'w')
 
-for el in liste:
-	print el.tag
-	result = el.tag, "\n"
-	outputFile.writelines(result)
+#for el in liste:
+#	print el.tag
+#	result = el.tag, "\n"
+#	outputFile.writelines(result)
 
-print "time for parsing the dump: ", end1 - start1, " sec"
-print "time for collecting all tags: ", end2 - start2, " sec"
+print "time for parsing the dump: ", parsingTime
+print "time for collecting all page elements: ", traversingTime
 		
 
 #elements = list(doc)
