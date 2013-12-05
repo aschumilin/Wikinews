d74646941b0e14b32f1aa147abd3d6714b149945
atsc <atsc@aifb-ls3-remus.aifb.kit.edu>
2013-12-05 05:38:48 +0100

    final parser

--- parser/parse.py
+++ parser/parse.py
@@ -3,29 +3,48 @@ from Timer import Timer
 import logging
 import sys
 import os
+import traceback
 
 
 # some fixed values
-#baseDir = "/home/aifb/stud/ls3/atsc/en_wikinews_2013-10-30/"
-baseDir = "/dev/shm/wikinews/" 
+
 elementName = "page"
 pageDistinguisher = "ns" # this child element of a page (here: wiki-internal-namespace) separates good articles from rubbish
+T = Timer() # stop watch to keep track of cpu time
 
+#baseDir = "/home/aifb/stud/ls3/atsc/en_wikinews_2013-10-30/"
+baseDir = "/dev/shm/wikinews/" 
 
 articleDirName = baseDir + "articles_pages/"
 junkDirName = baseDir + "other_pages/"
 logFileName = baseDir + "debug.log"
 dumpFileName = "/home/aifb/stud/ls3/atsc/en_wikinews_2013-10-30/enwikinews-20131030-pages-articles.xml"
 
-T = Timer() # stop watch to keep track of cpu time
+
 #===================
+
 
 handlerDebug = logging.FileHandler(logFileName)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handlerDebug.setFormatter(formatter)
 L = logging.getLogger("wikinews")
-L.setLevel(logging.DDEBUG)
+L.setLevel(logging.DEBUG)
 L.addHandler(handlerDebug)
+
+# 0
+# check if necessery dirs exist
+#
+if not os.path.exists(articleDirName):
+	L.debug("clean-article directory missing at " + articleDirName)
+	os.system("mkdir " + articleDirName)	
+	#sys.exit()
+if not os.path.exists(junkDirName):
+	L.debug("junk-page directory missing at " + junkDirName)
+	os.system("mkdir " + junkDirName)	
+	#sys.exit()
+if not os.path.exists(baseDir):
+	print "base directory missing at ", baseDir, "\n ...exiting script..."
+	sys.exit()
 
 
 
@@ -33,13 +52,15 @@ L.addHandler(handlerDebug)
 # 1 
 # parse dump and get the root of xml tree
 #
+L.debug("==============================================")
+L.debug("==============================================")
 L.debug("parsing started")
 T.click()
 dump = ET.parse(dumpFileName)
 wikinews = dump.getroot()
 T.click()
 parsingTime = T.show()
-L.debug("done parsing in: " + parsingTime)s
+L.debug("done parsing in: " + parsingTime)
 
 
 # 2 
@@ -83,40 +104,60 @@ for page in pages:
 			pageTitle = page.find(".//" + namespace + "title").text
 			pageID = page.find(".//" + namespace + "id").text
 			
-			targetFileName = pageID + "-" + pageTitle.replace(":", "#").replace(" ", "_")
+			targetFileName = constructTitle(pageID, pageTitle)
+
 			
-			targetFile = None
+			
+
 			if pageNS == 0: 
 				# this page is a regular Wikinews article
 				# save it appropriately
 				print targetFileName
 				targetFile = open(articleDirName + targetFileName, "w")
+				targetFile.write(ET.tostring(page, encoding="utf-8"))
+				targetFile.close()
 				numRegularArticles += 1
 			else:
 				# this page is not an article
 				# save it to the junk folder
 				targetFile = open(junkDirName + targetFileName, "w")
+				targetFile.write(ET.tostring(page, encoding="utf-8"))
+				targetFile.close()
 				numOtherPages += 1
 
-			targetFile.write(ET.tostring(page, encoding="utf-8")
-			targetFile.close()	
+				
 				
 		numProcessedPages += 1
 	except:
 		exceptionOccured = True
-		L.debug("failed at page " + targetFileName + "YYY Exception: " + sys.exc_info()[0])
+		L.debug("failed at " + targetFileName + " === Exception: " + str(sys.exc_info()[0]))
+		print "=== failed at page " , targetFileName , " === Exception: " , str(sys.exc_info()[0])
+		traceback.print_exc()
 T.click()
 
 pagesProcessingTime = T.show()
-L.debug("processing done in " + pagesProcessingTime + ":: 0 = " + str(numProcessedPages - numRegularArticles - numOtherPages))
+L.debug("processing done in " + pagesProcessingTime + " :: " + str(numRegularArticles) + " 0-wiki-namespace articles found")
 	
 
 print "dump parsed in ", parsingTime
 print "xpath pages in ", traversingTime
 print "process & save pages in ", pagesProcessingTime
+if exceptionOccured: print "es gab eine Exception bei page processing"
 
 # 5
 # move the final output out of /dev/shm to home dir
 
-if ("/dev/shm" in baseDir) & (not exceptionOccured):
+T.click()
+if ("/dev/shm" in baseDir):
+	L.debug("start copying to home/aifb/stud/ls3/atsc/en_wikinews_2013-10-30/")
 	os.system("cp -r /dev/shm/wikinews/* /home/aifb/stud/ls3/atsc/en_wikinews_2013-10-30/")
+T.click()
+L.debug("copying done in " + T.show())
+
+
+
+def constructFileName(pid, title):
+	fullName = pid + "-" + pageTitle.replace(" ", "_").replace(":", "#").replace("/", "-")
+	return fullName[0:50]
+	# first replace: title blanks, second: wiki-namespace colons, third: wiki-pages special chars 
+
